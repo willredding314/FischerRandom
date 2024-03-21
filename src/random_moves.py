f4b0c2b801cd70b819bb926c691cf96fd502ec36
houseparty101 <maxxtandon@gmail.com>
2024-03-21 15:47:47 -0400

    init src structure and create example chess env with random moves

---
+++ src/random_moves.py
@@ -0,0 +1,21 @@
+from pettingzoo.classic import chess_v6
+
+env = chess_v6.env(render_mode="human")
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+    print(reward)
+
+    if termination or truncation:
+        action = None
+    else:
+        mask = observation["action_mask"]
+
+    action = env.action_space(agent).sample(mask)
+
+    env.step(action)
+    input("next move")
+
+
+env.close()
